ccb530bc0f413338ade31c71062f7ed9d9800884
Vincent Lee <cool8511@gmail.com>
2019-08-12 14:39:42 +0800

    support more options for block based table

--- options_block_based_table.go
+++ options_block_based_table.go
@@ -4,6 +4,13 @@ package gorocksdb
 // #include "gorocksdb.h"
 import "C"
 
+const (
+	// the index type for blockbased table
+	IndexTypeBinarySearch int = iota
+	IndexTypeHashSearch
+	IndexTypeTwoLevelIndexSearch
+)
+
 // BlockBasedTableOptions represents block-based table options.
 type BlockBasedTableOptions struct {
 	c *C.rocksdb_block_based_table_options_t
@@ -63,6 +70,13 @@ func (opts *BlockBasedTableOptions) SetBlockRestartInterval(blockRestartInterval
 	C.rocksdb_block_based_options_set_block_restart_interval(opts.c, C.int(blockRestartInterval))
 }
 
+// SetIndexBlockRestartInterval sets the number of keys between
+// restart points for delta encoding of keys for index data block.
+// Default: 1
+func (opts *BlockBasedTableOptions) SetIndexBlockRestartInterval(restartInterval int) {
+	C.rocksdb_block_based_options_set_index_block_restart_interval(opts.c, C.int(restartInterval))
+}
+
 // SetFilterPolicy sets the filter policy opts reduce disk reads.
 // Many applications will benefit from passing the result of
 // NewBloomFilterPolicy() here.
@@ -120,4 +134,34 @@ func (opts *BlockBasedTableOptions) SetCacheIndexAndFilterBlocks(value bool) {
 
 func (opts *BlockBasedTableOptions) SetPinL0FilterAndIndexBlocksInCache(value bool) {
 	C.rocksdb_block_based_options_set_pin_l0_filter_and_index_blocks_in_cache(opts.c, boolToChar(value))
+}
+
+// SetMetaDataBlockSize sets the approximate size of index data packed per block.
+// Note that the block size specified here corresponds opts uncompressed data.
+// The actual size of the unit read from disk may be smaller if
+// compression is enabled. This parameter can be changed dynamically.
+// Default: 4K
+func (opts *BlockBasedTableOptions) SetMetaDataBlockSize(blockSize uint64) {
+	C.rocksdb_block_based_options_set_metadata_block_size(opts.c, C.uint64_t(blockSize))
+}
+
+func (opts *BlockBasedTableOptions) SetPartitionFilters(enable bool) {
+	C.rocksdb_block_based_options_set_partition_filters(opts.c, boolToChar(enable))
+}
+
+func (opts *BlockBasedTableOptions) SetIndexType(it int) {
+	//enum {
+	//	rocksdb_block_based_table_index_type_binary_search = 0,
+	//	rocksdb_block_based_table_index_type_hash_search = 1,
+	//	rocksdb_block_based_table_index_type_two_level_index_search = 2,
+	//  };
+	C.rocksdb_block_based_options_set_index_type(opts.c, C.int(it))
+}
+
+func (opts *BlockBasedTableOptions) SetCacheIndexAndFilterBlocksWithHighPriority(value bool) {
+	C.rocksdb_block_based_options_set_cache_index_and_filter_blocks_with_high_priority(opts.c, boolToChar(value))
+}
+
+func (opts *BlockBasedTableOptions) SetFormatVersion(value int) {
+	C.rocksdb_block_based_options_set_format_version(opts.c, C.int(value))
 }
